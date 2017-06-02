dd2b29fc32937d78c503837559eea525a5e5ac8a
nifrasismail <nifrasismail@gmail.com>
2017-06-02 14:23:10 +0530

    Android Sample Widget Implemented

--- android-tempalte/AndroidFeedBackAPITemplate/app/src/main/java/feedbackapi/com/androidfeedbackapitemplate/MainActivity.java
+++ android-tempalte/AndroidFeedBackAPITemplate/app/src/main/java/feedbackapi/com/androidfeedbackapitemplate/MainActivity.java
@@ -1,7 +1,12 @@
 package feedbackapi.com.androidfeedbackapitemplate;
 
+import android.support.annotation.FloatRange;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RatingBar ratingBar = new RatingBar(this);
+        ratingBar.setRating(0);
+        ratingBar.setStepSize(Float.parseFloat("0.5"));
+        ratingBar.setNumStars(5);
+        ratingBar.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        TextView textView = new TextView(this);
+        textView.setText("Nutrition");
+        textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        LinearLayout linearLayout = (LinearLayout)findViewById(R.id.ratingLayout);
+        linearLayout.addView(textView);
+        linearLayout.addView(ratingBar);
+
+        RatingBar ratingBar2 = new RatingBar(this);
+        ratingBar2.setRating(0);
+        ratingBar2.setStepSize(Float.parseFloat("0.5"));
+        ratingBar2.setNumStars(5);
+        ratingBar2.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        TextView textView2 = new TextView(this);
+        textView2.setText("Healthy Food");
+        textView2.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        linearLayout.addView(textView2);
+        linearLayout.addView(ratingBar2);
+
+
     }
 }
